fef7f6c670f4dd7d9c7aabcc90520834b77351c3
AliyaHeula <yakupovaliya@yandex.ru>
2020-09-10 16:00:48 +0300

    to be Normed

--- evalexpr/eval.c
+++ evalexpr/eval.c
@@ -28,12 +28,15 @@ void stacks_creation (char *str)
 	{
 		if (str[i] >= '0' && str[i] <= '9')
 			n++;
-		if (str[i] >= '%' && str[i] <= '/')
+		if (str[i] == '+' || str[i] == '-' || str[i] == '*' ||
+		str[i] == '/' || str[i] == '%' || str[i] == '(' ||
+		str[i] == ')' || str[i] == ' ')
 			c++;
 		i++;
 	}
-	g_nums = (int *)malloc(sizeof(int) * n);
-	g_ops = (char *)malloc(sizeof(char) * c + 1);
+	if ((g_nums = (int *)malloc(sizeof(int) * n)) == NULL ||
+	(g_ops = (char *)malloc(sizeof(char) * c + 1)) == NULL)
+		write(1, "Memory error\n", 13);
 }
 
 void	read_write(char *str)
@@ -47,44 +50,57 @@ void	read_write(char *str)
 	k = 0;
 	while (str[k] != '\0')
 	{
-		// WARNING!!! Dalshe mozhno slomat' glaza!
-		// Indusskiy kod, Alilluya!
-		if ((str[k] == '-' && k == 0) || (str[k] == '-' && str[k - 1] == '(') ||
-		(str[k] >= '0' && str[k] <= '9'))
-			k += ft_evalatoi(&str[k], ++i);
-		while (str[k] == '+' || str[k] == '-' || str[k] == '*' || str[k] == '/' ||
-		str[k] == '%' || str[k] == '(' || str[k] == ')')
-		{
-			if ((str[k] == '(' || str[k - 1] == '(' || j == -1) ||
-			((str[k] == '+' || str[k] == '-') && (g_ops[j] == '(')) || 
-			((str[k] == '%'|| str[k] == '*' || str[k] == '/') && (g_ops[j] == '+'
-				|| g_ops[j] == '-' || g_ops[j] == '(')))
-			{
-				g_ops[++j] = str[k++];
-			}
-			else
-			{
-				if ((str[k] == ')' && g_ops[j] != '(') || j >= 0)
-					calculator ((i--), (j--));
-				if (g_ops[j] == '(' && str[k] == ')')
-				{
-					j--;
-					k++;
-				}
-			}
-			if (str[k] == '-' && str[k - 1] == '(')
-			{	j--;
-				break;
-			}	
-		}
 		if (str[k] == ' ')
 			k++;
-		if (str[k] == '\0')
+	//	printf("____________________k = %d\n", k);
+	//	printf("START str[%d] %s\n", k, &str[k]); 
+	//	printf("START g_nums[%d] %d\n", i, g_nums[i]); 
+	//	printf("START g_ops[%d] %s\n", j, &g_ops[j]); 
+	// WARNING!!! Dalshe mozhno slomat' glaza!
+	// Indusskiy kod, Alilluya!
+	// "86-47*(-26-75/(47-98/23)+17/(-86+44*38))"
+	//"3 + 42 * (1 - 2 / (3 + 4) - 1 % 21) + 1"
+		if ((str[k] == '-' && k == 0) || (str[k] == '-' &&
+		str[k - 1] == '(') || (str[k] >= '0' && str[k] <= '9'))
 		{
-			while (i > 0)
-				calculator((i--), (j--)); 
-			continue;
+			k += ft_evalatoi(&str[k], ++i);
+		//	printf("write g_nums[%d] %d\n", i, g_nums[i]);
+		//	printf("k = %d\n", k);
+			if (str[k] != '\0')
+				continue;	
 		}
+		if ((str[k] == '(' || str[k - 1] == '(' || j == -1) ||
+		((str[k] == '+' || str[k] == '-') && (g_ops[j] == '(')) || 
+		((str[k] == '%'|| str[k] == '*' || str[k] == '/') &&
+		(g_ops[j] == '+' || g_ops[j] == '-' || g_ops[j] == '(')))
+		{
+			g_ops[++j] = str[k++];
+	//		printf("write g_ops[%d] %s\n", j, &g_ops[j]); 
+		}
+		else
+		{
+		// while((str[k] == ')' && g_ops[j] != '(') || j >= 0)
+			while (g_ops[j] != '(' && j >= 0)
+			{
+			//	printf("calc g_nums[%d] %d\n", i, g_nums[i]); 
+			//	printf("calc g_nums[%d] %d\n", (i - 1), g_nums[i - 1]); 
+			//	printf("calc g_ops[%d] %s\n", j, &g_ops[j]); 
+				calculator ((i--), (j--));
+			//	printf("%s\n", &str[0]);
+			}
+			if (g_ops[j] == '(' && str[k] == ')')
+			{
+				j--;
+				k++;
+			//	printf("here\n");
+			}
+		}
+			if (str[k] == '\0')
+	//		{
+	//			printf("check\n");
+				while (i > 0)
+					calculator((i--), (j--)); 
+	//		}
 	}
 }
 
@@ -106,7 +122,7 @@ int	ft_evalatoi(char *str, int i)
 	}
 	while (str[k] >= '0' && str[k] <= '9')
 		k++;
-	while (k-- > 1)
+	while ((--k >= 1 && minus == 1) || (k >= 2 && minus == -1))
 		power *= 10;
 	while (str[k] >= '0' && str[k] <= '9')
 	{
